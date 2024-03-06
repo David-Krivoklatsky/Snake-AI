cabf7307b98d2fa1231dddc51f1fd22769a32c11
David Krivoklatsky <dado70dado70@gmail.com>
2024-03-06 12:12:59 +0100

    random jumping circle :D

--- Snake-AI/Main.cpp
+++ Snake-AI/Main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SnakeAI");
+    sf::CircleShape shape(100);
     shape.setFillColor(sf::Color::Green);
+
 
     while (window.isOpen())
     {
@@ -17,7 +20,13 @@ int main()
         }
 
         window.clear();
+
+
+        shape.setPosition(50 + rand() % 601, 50 + rand() % 601);
         window.draw(shape);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        
         window.display();
     }
 
