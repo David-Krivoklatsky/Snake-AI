3149d48aaa9a80eb15d13b46f023947b202deec0
David Krivoklatsky <dado70dado70@gmail.com>
2024-03-09 15:51:03 +0100

    added fps and boders

--- Snake-AI/Main.cpp
+++ Snake-AI/Main.cpp
@@ -66,6 +66,7 @@ void drawFood(sf::RenderWindow& window, sf::RectangleShape& block, const sf::Vec
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE, WINDOW_SIZE), "SnakeAI");
+    window.setFramerateLimit(60);
     sf::RectangleShape block(sf::Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 
     std::vector<sf::Vector2f> snake;
@@ -75,6 +76,19 @@ int main()
     int changeX = 0, changeY = 0; //koeficients of changing position to the left or right, up or down
 
     sf::Vector2f food = generateFood(snake);
+
+    //time vars for fps count
+    std::chrono::high_resolution_clock::time_point last_time, now = std::chrono::high_resolution_clock::now();;
+    float fps;
+
+    sf::Font font;
+    if (!font.loadFromFile("font.ttf")) {
+        return EXIT_FAILURE;
+    }
+
+    sf::Text fps_text("Hello, SFML!", font, 24);
+    fps_text.setFillColor(sf::Color::White);
+    fps_text.setPosition(0, 0);
 
     while (window.isOpen())
     {
@@ -113,23 +127,35 @@ int main()
 
         //move snake
         nextPos = sf::Vector2f(snake[0].x + changeX, snake[0].y + changeY);
-        snake.insert(snake.begin(), nextPos);
+        if (nextPos.x >= 0 && nextPos.x < WINDOW_SIZE && nextPos.y >= 0 && nextPos.y < WINDOW_SIZE) {
 
-        if (food == snake[0]) {
-            food = generateFood(snake);
+            snake.insert(snake.begin(), nextPos);
+
+            if (food == snake[0]) {
+                food = generateFood(snake);
+            }
+            else {
+                snake.pop_back();
+            }
         }
-        else {
-            snake.pop_back();
-        }
+
+        //fps count
+        last_time = now;
+        now = std::chrono::high_resolution_clock::now();
+        fps = 1.0f / std::chrono::duration_cast<std::chrono::duration<float>>(now - last_time).count();
+
+
+        fps_text.setString(std::to_string(fps));
 
         window.clear();
 
         drawGrid(window, block);
         drawSnake(window, block, snake);
         drawFood(window, block, food);
+        window.draw(fps_text);
 
         //sleep
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(200));
         
         window.display();
     }
