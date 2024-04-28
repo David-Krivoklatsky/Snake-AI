3c9f6d886c61c6008ddc9c2df1ca767bb3a9dee4
Milan Loduha <minoloduha@gmail.com>
2024-04-28 14:11:46 +0200

    backprop

--- Snake-AI/NeuralNetwork.cpp
+++ Snake-AI/NeuralNetwork.cpp
@@ -61,3 +61,63 @@ std::vector<double> NeuralNetwork::forwardPass(const std::vector<double>& inputs
     }
     return output;
 }
+void NeuralNetwork::backprop(const std::vector<double>& input, const std::vector<double>& expected, std::vector<std::vector<double>>& nablaB, std::vector<std::vector<std::vector<double>>>& nablaW) const {
+	std::vector<std::vector<double>> activations;
+	std::vector<std::vector<double>> zs;
+
+	activations.resize(numLayers);
+	zs.resize(numLayers);
+
+	for (size_t i = 0; i < numLayers; i++)
+	{
+		activations[i].resize(layers[i]);
+		zs[i].resize(layers[i]);
+	}
+
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		activations[0][i] = input[i];
+	}
+
+	for (size_t i = 1; i < numLayers; i++)
+	{
+		for (size_t j = 0; j < layers[i]; j++)
+		{
+			double temp_value = 0;
+			for (size_t k = 0; k < layers[i - 1]; k++)
+			{
+				temp_value += activations[i - 1][k] * net[i][j].input_weights[k];
+			}
+			temp_value += net[i][j].bias;
+
+			zs[i][j] = temp_value;
+			activations[i][j] = sigmoid(temp_value);
+		}
+	}
+
+	std::vector<std::vector<double>> delta;
+	delta.resize(numLayers);
+
+	for (size_t i = 0; i < numLayers; i++)
+	{
+		delta[i].resize(layers[i]);
+	}
+
+	for (size_t i = 0; i < layers.back(); i++)
+	{
+		delta.back()[i] = (activations.back()[i] - expected[i]) * sigmoidPrime(zs.back()[i]);
+	}
+
+	for (size_t i = numLayers - 2; i > 0; i--)
+	{
+		for (size_t j = 0; j < layers[i]; j++)
+		{
+			double temp_value = 0;
+			for (size_t k = 0; k < layers[i + 1]; k++)
+			{
+				temp_value += delta[i + 1][k] * net[i + 1][k].input_weights[j];
+			}
+			delta[i][j] = temp_value * sigmoidPrime(zs[i][j]);
+		}
+	}
+}
