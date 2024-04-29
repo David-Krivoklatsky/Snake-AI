f5bb01e12d871e3d93bec46b47d422a201c59e2b
David Krivoklatsky <dado70dado70@gmail.com>
2024-04-29 16:01:03 +0200

    deleted useless includes

--- Snake-AI/Main.cpp
+++ Snake-AI/Main.cpp
@@ -1,18 +1,9 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <SFML/Graphics.hpp>
 #include "SnakeGame.hpp"
 
 int main() {
     SnakeGame game;
 
     game.run();
-
-    /*while (!game.restartGame) {
-        game.run();
-        game.retryMenu();
-    }*/
 
     return 0;
 }
