aaaa3d5d9e9e49d7958c2c0bed0d70f3445560e9
David Krivoklatsky <dado70dado70@gmail.com>
2024-05-07 11:52:13 +0200

    slower snake

--- Snake-AI/Globals.hpp
+++ Snake-AI/Globals.hpp
@@ -6,7 +6,7 @@ constexpr int WINDOW_SIZE = 800;
 constexpr int PIXEL_SIZE = 20;
 constexpr int BLOCK_SIZE = WINDOW_SIZE / PIXEL_SIZE;
 constexpr int FPS_LIMIT = 60;
-constexpr float SNAKE_SPEED = 7.f;
+constexpr float SNAKE_SPEED = 5.f;
 
 enum Direction {
 	Up = 0,
