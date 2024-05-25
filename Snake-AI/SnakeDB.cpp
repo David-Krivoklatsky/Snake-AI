257cb4496d652b5eb60618acd81c8dafaa354cbb
Milan Loduha <minoloduha@gmail.com>
2024-05-25 08:34:08 +0200

    whereas score isn't displaying while you're playing AI-noob etc... It crash beecause score is NULL

--- Snake-AI/SnakeDB.cpp
+++ Snake-AI/SnakeDB.cpp
@@ -8,9 +8,10 @@ void DB::connectDB(sql::mysql::MySQL_Driver* driver) {
 }
 
 
-void DB::noteScore(int score, const std::string& nick, std::string &mode) {
+void DB::noteScore(int score, std::string nick, std::string &mode) {
 	stmt = con->createStatement();
 	std::string query = "INSERT INTO scoreboard (Nick, Score, Mode) VALUES (trim('" + nick + "'), " + std::to_string(score) + ",'" + mode + "')";
+	//nick = ""; chcel som aby sa to mazalo tuto ale nefunguje to
 	std::cout << query << std::endl;
 	try {
 		stmt->execute(query);
--- Snake-AI/SnakeDB.hpp
+++ Snake-AI/SnakeDB.hpp
@@ -8,7 +8,7 @@
 class DB {
 public:
     void connectDB(sql::mysql::MySQL_Driver* driver);
-    void noteScore(int score, const std::string& , std::string&);
+    void noteScore(int score, std::string , std::string&);
 private:
     sql::mysql::MySQL_Driver* driver;
     sql::Connection* con;
